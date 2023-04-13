bb1b19e7be9dcabb52162b6b5c5dcb9a20f75ce9
Cloud User <sami@vm.tuafzu1hoxauvgvrnliqcwha2h.jx.internal.cloudapp.net>
2023-04-13 14:18:43 +0000

    change header of webapp

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home</h3>"
+    html = "<h1>Sami Andrianantenaina</h1><h3>Sklearn Prediction Home</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
